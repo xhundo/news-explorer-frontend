e681a1912ac751d01c0b5873cb763178f87f307a
Kurtney Joseph <josephkurt18@gmail.com>
2023-01-05 17:42:54 -0500

    add media queries

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,18 +6,22 @@ function Footer({ changeTheme }) {
     <footer className="footer">
       <p className="footer__text">© 2021 Supersite, Powered by News API</p>
       <div className="footer__nav">
-        <Link onClick={changeTheme} to={'/'} className="footer__links">
-          Home
-        </Link>
-        <a className="footer__links" href="https://practicum.com/">
-          Practicum
-        </a>
-        <a
-          className="footer__icon-git"
-          href="https://github.com/xhundo/news-explorer-frontend.git"
-          alt="Github"
-        ></a>
-        <a className="footer__icon-facebook" alt="Facebook"></a>
+        <div className="footer-nav__links">
+          <Link onClick={changeTheme} to={'/'} className="footer__links">
+            Home
+          </Link>
+          <a className="footer__links" href="https://practicum.com/">
+            Practicum
+          </a>
+        </div>
+        <div className="footer__icons">
+          <a
+            className="footer__icon-git"
+            href="https://github.com/xhundo/news-explorer-frontend.git"
+            alt="Github"
+          ></a>
+          <a className="footer__icon-facebook" alt="Facebook"></a>
+        </div>
       </div>
     </footer>
   );
--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -10,8 +10,12 @@ function SavedNewsHeader() {
       <p className="saved-news__tab-title">Saved articles</p>
       <h2 className="saved-news__title">{`${currentUser.name}, you have 5 saved articles`}</h2>
       <div className="saved-news__key">
-        <p className="keywords__title">By keywords:</p>
-        <p className="saved-news__keywords">Nature, Yellowstone, and 2 other</p>
+        <p className="saved-keywords__title">
+          By keywords:
+          <span className="saved-news__keywords">
+            Nature, Yellowstone, and 2 other
+          </span>
+        </p>
       </div>
     </header>
   );
