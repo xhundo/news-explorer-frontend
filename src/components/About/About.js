7cde9862b19b907ec2a6dd082b2b81867f45a61c
Kurtney Joseph <josephkurt18@gmail.com>
2023-02-05 20:59:18 -0500

    feat: add author content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,11 @@
 import './About.css';
-import author from '../../images/image-03.jpg';
+import author from '../../images/author.jpg';
+
+/* This block describes the project author. Here you should indicate your
+name, what you do, and which development technologies you know. */
+
+// You can also talk about your experience with Practicum, what you
+// learned there, and how you can help potential customers.
 
 function About() {
   return (
@@ -8,12 +14,19 @@ function About() {
       <div className="about__author-text">
         <h2 className="about__text">About the author</h2>
         <article className="about__article">
-          This block describes the project author. Here you should indicate your
-          name, what you do, and which development technologies you know.
+          Kurtney Joseph is a full-stack software engineer experienced with a
+          proficient foundation in JavaScript, React and Node.js and is highly
+          driven to constantly learn new technologies to provide scalable,
+          cutting-edge, and maintable software solutions to effectivly build,
+          and scale teams for companies and organizations.
         </article>
         <article className="about__article">
-          You can also talk about your experience with Practicum, what you
-          learned there, and how you can help potential customers.
+          Practicum was a great experience and opportunity to build my skills to
+          bring value to teams and organizations. I've learned over 740+ hours
+          of HTML, CSS, JavaScript, React, Express, Node, and MongoDB. These
+          skills have tremendously helped me scale teams, build better
+          development enviorments and to provide cutting-edge web applications
+          to serve users a great web experience.
         </article>
       </div>
     </section>
