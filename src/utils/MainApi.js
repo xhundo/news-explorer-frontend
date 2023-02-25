3d2d8166d98fe33533b2a21ab022580978bd9d88
Kurtney Joseph <josephkurt18@gmail.com>
2023-02-25 17:25:22 -0500

    refactor: reverted from class

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,90 +1,93 @@
-class Api {
-  constructor({ baseUrl, headers }) {
-    this.baseUrl = baseUrl;
-    this.headers = headers;
+import { baseUrl } from './constants';
+
+const checkRes = (res) => {
+  if (res.ok) {
+    return res.json();
+  } else {
+    Promise.reject(`Error: ${res.status}`);
   }
+};
 
-  getCurrentUser = () => {
-    return fetch(`${this.baseUrl}/users/me`, {
-      headers: this.headers,
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      Promise.reject(`Error: ${res.status}`);
-    });
-  };
+const getCurrentUser = () => {
+  return fetch(`${baseUrl}/users/me`, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  }).then(checkRes);
+};
 
-  createUser = (email, password, name) => {
-    return fetch(`${this.baseUrl}/signup`, {
-      method: 'POST',
-      headers: this.headers,
-      body: JSON.stringify({ email, password, name }),
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw new Error(`Error: ${res.status}`);
-    });
-  };
+const createUser = (email, password, name) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+    body: JSON.stringify({ email, password, name }),
+  }).then(checkRes);
+};
 
-  loginUser = (email, password) => {
-    return fetch(`${this.baseUrl}/signin`, {
-      method: 'POST',
-      headers: this.headers,
-      body: JSON.stringify({ email, password }),
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        Promise.reject(`Error: ${res.status}`);
-      })
-      .then((data) => {
-        if (data.token) {
-          localStorage.setItem('token', data.token);
-          return data;
-        }
-      });
-  };
-
-  getArticles = () => {
-    return fetch(`${this.baseUrl}/articles`, {
-      method: 'GET',
-      headers: this.headers,
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        Promise.reject(`Error: ${res.status}`);
+const loginUser = (email, password) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then(checkRes)
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem('token', data.token);
+        return data;
       }
     });
-  };
+};
 
-  deleteArticle = (id) => {
-    return fetch(`${this.baseUrl}/articles/${id}`, {
-      method: 'DELETE',
-      headers: this.headers,
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      Promise.reject(`Error: ${res.status}`);
-    });
-  };
+const getArticles = () => {
+  return fetch(`${baseUrl}/articles`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  }).then(checkRes);
+};
 
-  createArticle = (keyword, title, text, date, source, link, image) => {
-    return fetch(`${this.baseUrl}/articles`, {
-      method: 'POST',
-      headers: this.headers,
-      body: JSON.stringify({ keyword, title, text, date, source, link, image }),
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        Promise.reject(`Error: ${res.status}`);
-      }
-    });
-  };
-}
-export default Api;
+const deleteArticle = (id) => {
+  return fetch(`${baseUrl}/articles/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  }).then(checkRes);
+};
+
+const createArticle = (keyword, title, text, date, source, link, image) => {
+  return fetch(`${baseUrl}/articles`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+    body: JSON.stringify({ keyword, title, text, date, source, link, image }),
+  }).then(checkRes);
+};
+
+export {
+  getCurrentUser,
+  createUser,
+  loginUser,
+  getArticles,
+  deleteArticle,
+  createArticle,
+};
